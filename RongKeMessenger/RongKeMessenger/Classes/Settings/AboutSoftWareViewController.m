94de1624e733627904aa42a5df6dee8ec444410c
rongke <yetaozhu@rongkecloud.com>
2016-08-24 11:21:04 +0800

    -- 修改关于界面背景色为预设的界面背景色，并优化界面布局代码。

--- RongKeMessenger/RongKeMessenger/Classes/Settings/AboutSoftWareViewController.m
+++ RongKeMessenger/RongKeMessenger/Classes/Settings/AboutSoftWareViewController.m
@@ -11,22 +11,24 @@
 
 @interface AboutSoftWareViewController ()
 
-@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-@property (nonatomic, weak) IBOutlet UIImageView *logoImageView;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView; // 整个页面的 scrollView
+@property (nonatomic, weak) IBOutlet UIImageView *logoImageView; // logo 图片
 @property (nonatomic, weak) IBOutlet UILabel *labelVersion; // 版本号显示
-@property (weak, nonatomic) IBOutlet UITextView *detailTextView;
-@property (weak, nonatomic) IBOutlet UILabel *rightsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rightsLabel; // 权利声明 label
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *spaceBetweenConnectAndCompany; // 联系方式 textView 的底部和公司名 label 的顶部之间的高度
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *detailTextViewWidth; // 应用详情 textView 的宽度
 
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *spaceBetweenConnectAndCompany;
-
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *detailTextViewWidth;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *spaceOfLeftEdge; // 左边距
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *spaceOfRightEdge; // 右边距
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *spaceOfBottomEdge; // 底边距
 
 @end
 
 @implementation AboutSoftWareViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
@@ -38,18 +40,20 @@
     
     self.labelVersion.text = [NSString stringWithFormat:@"%@(V%@)", APP_DISPLAY_NAME, APP_WHOLE_VERSION];
     
-    self.detailTextViewWidth.constant = UISCREEN_BOUNDS_SIZE.width - 2 * 8;
+    self.detailTextViewWidth.constant = UISCREEN_BOUNDS_SIZE.width - (self.spaceOfLeftEdge.constant + self.spaceOfBottomEdge.constant);
 }
 
--(void)viewDidLayoutSubviews
+// 在较大屏幕设备上让最下面两个 label 能接近底部显示
+// 用 viewDidLayoutSubviews 而不用 viewWillAppear 或 viewDidLoad 的原因是后两者并没有开始 layout，所以 scrollView 的高度并没有适配当前设备
+- (void)viewDidLayoutSubviews
 {
-    NSLog(@"%f, %f ,%f",CGRectGetMaxY(self.rightsLabel.frame), self.scrollView.frame.size.height, self.scrollView.contentSize.height);
-    if (self.scrollView.frame.size.height - CGRectGetMaxY(self.rightsLabel.frame) > 8)
+    // 此处使用 scrollView 的高而不是其 contentSize 的高的原因是后者的高为 0
+    CGFloat heightWillAdd = self.scrollView.frame.size.height - CGRectGetMaxY(self.rightsLabel.frame) - self.spaceOfBottomEdge.constant;
+    
+    if (heightWillAdd > 0)
     {
-        self.spaceBetweenConnectAndCompany.constant += self.scrollView.frame.size.height - CGRectGetMaxY(self.rightsLabel.frame) - 8;
+        self.spaceBetweenConnectAndCompany.constant += heightWillAdd;
     }
-    NSLog(@"%f, %f ,%f",CGRectGetMaxY(self.rightsLabel.frame), self.scrollView.frame.size.height, self.scrollView.contentSize.height);
-
 }
 
 - (void)didReceiveMemoryWarning {
